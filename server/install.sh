ee3ff88687a07104e80e225bd46b34f871cfa40e
Nicolae Postica <nicolae.postica2@gmail.com>
2025-10-22 23:18:33 +0300

    refactor: remove redundant sudo commands from installation script and ensure proper superuser handling

--- server/install.sh
+++ server/install.sh
@@ -1,7 +1,15 @@
-sudo mkdir -p /usr/local/bin/procsentinel
-sudo tar -xzvf procsentinel-server.tar.gz -C /usr/local/bin/procsentinel
-sudo cp /usr/local/bin/procsentinel/procsentinel.service /etc/systemd/system/procsentinel.service
-sudo systemctl daemon-reload
-sudo systemctl enable procsentinel
-sudo systemctl start procsentinel
-sudo systemctl status procsentinel
+#!/bin/bash
+# Requires superuser privileges
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Switching to root..."
+  exec sudo -i bash "$0" "$@"
+fi
+
+mkdir -p /usr/local/bin/procsentinel
+tar -xzvf procsentinel-server.tar.gz -C /usr/local/bin/procsentinel
+cp /usr/local/bin/procsentinel/procsentinel.service /etc/systemd/system/procsentinel.service
+systemctl daemon-reload
+systemctl enable procsentinel
+systemctl start procsentinel
+systemctl status procsentinel
